f96a181a9527f62dde670e810a9c7ac18a762f71
0211bio038 <102760989+0211bio038@users.noreply.github.com>
2023-03-16 17:36:19 +0530

    Update sum_of_n_natural_numbers.cpp

--- Recursion/sum_of_n_natural_numbers.cpp
+++ Recursion/sum_of_n_natural_numbers.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
 
+int SumofNatural(int a);
+
+int main()
+{
+  int n;
+  cout<<"Enter a number till which you want the sum: ";
+  cin>>n;
+  cout<<endl;
+  cout<<"The sum of first "<<n<<"Number is: "<<SumofNatural(n)<<endl;
+  return 0;
+}
+
+int SumofNatural(int a)
+{
+  if(a==1)
+  {
+    return 1;
+  }
+  else{
+    return a + SumofNatural(a-1);
+  }
+}
