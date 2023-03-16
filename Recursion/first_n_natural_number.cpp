fe1cb7861d8671742ea60c40a9c4f5e7a6cb3ecb
0211bio038 <102760989+0211bio038@users.noreply.github.com>
2023-03-16 20:08:14 +0530

    Create first_n_natural_number.cpp

---
+++ Recursion/first_n_natural_number.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int NaturalNumber(int a);
+
+int main()
+{
+    int n;
+    cout<<"Enter a number till which you want to print Natural Numbers: ";
+    cin>>n;
+    NaturalNumber(n);
+    return 0;
+}
+
+int NaturalNumber(int a)
+{
+    if(a==1)
+    {
+        cout<<"1"<<" ";
+    }
+    else{
+        NaturalNumber(a-1);
+        cout<<a<<" ";
+    }
+  return 0;
+}
