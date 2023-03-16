d3b83b9f773dd0a5be78b7fafff82e13b4e5ed95
0211bio038 <102760989+0211bio038@users.noreply.github.com>
2023-03-16 17:58:08 +0530

    Create prime_number.cpp

---
+++ Function/prime_number.cpp
@@ -0,0 +1,35 @@
+//Check if the given number is Prime or Not.
+
+#include <iostream>
+using namespace std;
+
+bool PrimeNumber(int a);
+
+int main()
+{
+    int n;
+    cout<<"Enter a number to check it is a prime or not: ";
+    cin>>n;
+    if(PrimeNumber(n)==true)
+    {
+        cout<<n<<" is a Prime Number."<<endl;
+    }
+    else{
+        cout<<n<<" is NOT a Prime Number."<<endl;
+    }
+    return 0;
+}
+
+bool PrimeNumber(int a)
+{
+    bool isFlag=true;
+    for(int i=2; i<a; i++)
+    {
+        if(a%i==0)
+        {
+            isFlag=false;
+            break;
+        }
+    }
+    return isFlag;
+}
